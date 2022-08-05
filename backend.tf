427c80c87c1324ef8627f92d52a19082ea9bea8c
Kahmoma <99199589+Kahmoma@users.noreply.github.com>
2022-08-04 19:06:08 -0500

    Update backend.tf

--- backend.tf
+++ backend.tf
@@ -1,7 +1,7 @@
    terraform {
    backend "s3" {
    bucket = "code-sharing-bucket"
-   key    = "avinash/s3file-3.tfstate"
+   key    = "kah/s3file-3.tfstate"
    region = "us-west-2"
    profile = "kah"
 dynamodb_table= "firstprojecttable"
